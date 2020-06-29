be6af51c98a23b5819d951ba705f7daf9f78e6a5
Luis Alfonso <Luis Enrique Alfonso>
2020-06-29 12:35:13 -0500

    added new levels

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,7 +14,8 @@ export class HomePage implements OnInit {
   levels = [
     {
       title: "Crea una variable",
-      description: 'Escribe "Sea \'x\'. y selecciona Ejecutar Código ',
+      description: 'Escribe "Sea \'x\'." y selecciona Ejecutar Código ',
+      hint: 'Así como en el español, el punto al final de la sentencia es importante.',
       winning: (response) => {
         console.log(response)
         if (response.response.vars) {
@@ -42,6 +43,7 @@ export class HomePage implements OnInit {
     {
       title: "Asigna un valor a la variable",
       description: 'Asígnale a x el valor de 1 escribiendo "Sea \'x\' igual al número 1." y luego selecciona Ejecutar Código. ',
+      hint: '¡Respeta los espacios! No necesitas más ni menos. Si el corrector ortográfico muestra errores: tu código podría estar mal.',
       winning: (response) => {
         console.log(response)
         if (response.response.vars) {
@@ -66,6 +68,65 @@ export class HomePage implements OnInit {
           }
         }
       }
+    },
+    {
+      title: "Haz que tu programa 'Hable'",
+      description: 'Crea una variable y asígnale un valor numérico; luego muestralo en el terminal usando "Imprimir \'MiVariable\'." ',
+      hint: 'Al escribir varias sentencias, sepáralas usando ".". Si es más comodo: cada sentencia puede ir en una línea aparte ;)',
+      winning: (response) => {
+        console.log(response)
+        if (response.response.vars) {
+          this.debug = response.response.vars
+          if (response.response.printStream.length > 0) {
+            this.terminal += " ¡Felicitaciones! pasas al siguiente nivel :D \n >:"
+            this.disableNextLevel = false
+            console.log("yes")
+          } else if (response.response.error === 1) {
+            this.terminal += " Error Sintáctico: verifica la sintaxis de tu programa \n >:"
+          } else if (response.response.error) {
+            this.terminal += ` Error Semántico: ${response.response.error}  \n >:`
+          } else {
+            this.terminal += " Error: no has imprimido tu variable\n >:"
+          }
+        } else {
+          if (response.response.error === 1) {
+            this.terminal += " Error Sintáctico: verifica la sintaxis de tu programa \n >:"
+          }
+          if (response.response.error) {
+            this.terminal += " Error Semántico: verifica tu programa \n >:"
+          }
+        }
+      }
+    },
+    {
+      title: "Suma números",
+      description: 'Crea una variable sin valor, luego crea dos variables y asígnales valores numéricos; sumalos y asígnalos a la primera usando "Haga que \'primera\' sea igual al número \'segunda\' más \'tercera\'."',
+      hint: '"Sea" y "haga" son distintos: la primera define una variable y la segunda cambia su valor',
+      winning: (response) => {
+        console.log(response)
+        if (response.response.vars) {
+          this.debug = response.response.vars
+          console.log(Object.keys(response.response.vars).length)
+          if (Object.keys(response.response.vars).length === 3) {
+            this.terminal += " ¡Felicitaciones! pasas al siguiente nivel :D \n >:"
+            this.disableNextLevel = false
+            console.log("yes")
+          } else if (response.response.error === 1) {
+            this.terminal += " Error Sintáctico: verifica la sintaxis de tu programa \n >:"
+          } else if (response.response.error) {
+            this.terminal += ` Error Semántico: ${response.response.error}  \n >:`
+          } else {
+            this.terminal += " Error: no has imprimido tu variable\n >:"
+          }
+        } else {
+          if (response.response.error === 1) {
+            this.terminal += " Error Sintáctico: verifica la sintaxis de tu programa \n >:"
+          }
+          if (response.response.error) {
+            this.terminal += " Error Semántico: verifica tu programa \n >:"
+          }
+        }
+      }
     }
   ]
   currentLevel = 1
@@ -77,8 +138,12 @@ export class HomePage implements OnInit {
   }
   async sendCode(form) {
     console.log(form.value)
-    let response = await this.connection.sendCode(this.cleanCode(form.value))
-    console.log(response)
+    let response:any = await this.connection.sendCode(this.cleanCode(form.value))
+    if(response.response.printStream && response.response.printStream.length > 0){
+      response.response.printStream.forEach(element=>{
+        this.terminal += ` ${element.value? element.value: "Indefinido"}  \n >:`
+      })
+    }
     this.levels[this.currentLevel - 1].winning(response)
   }
 
